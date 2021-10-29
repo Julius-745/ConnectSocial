7e4637ba0b0e267bd778f71bfc90dd3f0f800319
Julius-745 <juliusagung123@gmail.com>
2021-10-29 22:31:36 +0700

    base navigation

--- app/src/main/java/com/julius745/connect/MainActivity.java
+++ app/src/main/java/com/julius745/connect/MainActivity.java
@@ -3,12 +3,17 @@ package com.julius745.connect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 }
