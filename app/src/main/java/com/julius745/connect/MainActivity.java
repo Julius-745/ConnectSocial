ea3cb1b9c5275f0a9247037f6ff7723c9dac690c
Julius-745 <juliusagung123@gmail.com>
2021-11-09 01:38:17 +0700

    navigation bar fix

--- app/src/main/java/com/julius745/connect/MainActivity.java
+++ app/src/main/java/com/julius745/connect/MainActivity.java
@@ -1,15 +1,54 @@
 package com.julius745.connect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.julius745.connect.fragment.AddpostFragment;
+import com.julius745.connect.fragment.ContentFragment;
+import com.julius745.connect.fragment.SettingsFragment;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.dashboard);
     }
+    ContentFragment contentFragment = new ContentFragment();
+    AddpostFragment addpostFragment = new AddpostFragment();
+    SettingsFragment settingsFragment = new SettingsFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.dashboard:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, contentFragment).commit();
+                return true;
+            case R.id.create:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, addpostFragment).commit();
+                return true;
+            case R.id.settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, settingsFragment).commit();
+                return true;
+        }
+        return false;
+    }
+
 }
