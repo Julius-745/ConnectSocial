bff72ca5d5115a0ba41eeeac78cdd5a6c2812b6f
Julius-745 <juliusagung123@gmail.com>
2021-12-09 12:39:42 +0700

    locale

--- app/src/main/java/com/julius745/connect/fragment/SettingsFragment.java
+++ app/src/main/java/com/julius745/connect/fragment/SettingsFragment.java
@@ -1,17 +1,22 @@
 package com.julius745.connect.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.julius745.connect.R;
 
 public class SettingsFragment extends Fragment {
-
+    Button btnlanguage;
     public SettingsFragment() {
         // Required empty public constructor
     }
@@ -22,4 +27,20 @@ public class SettingsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstance){
+        super.onViewCreated(view, savedInstance);
+        btnlanguage = view.findViewById(R.id.btn_languange);
+
+        btnlanguage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 }
