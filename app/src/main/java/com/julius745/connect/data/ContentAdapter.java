110cfbc70206faf1dbfe35b7e78dbc414404ff02
Wildan M <willnode@wellosoft.net>
2021-12-09 13:02:02 +0700

    Upload content list

---
+++ app/src/main/java/com/julius745/connect/data/ContentAdapter.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.julius745.connect.data;
+
+
+import android.content.Intent;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.julius745.connect.R;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Provide views to RecyclerView with data from mDataSet.
+ */
+public class ContentAdapter extends RecyclerView.Adapter<ContentAdapter.ViewHolder> {
+    private static final String TAG = "MenuItemAdapter";
+
+    private List<Map> mDataSet;
+
+
+    // BEGIN_INCLUDE(recyclerViewSampleViewHolder)
+    /**
+     * Provide a reference to the type of views that you are using (custom ViewHolder)
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public Map data;
+        public final TextView titleView;
+        public final TextView contentView;
+        public final ImageView imageView;
+
+        public ViewHolder(View v) {
+            super(v);
+            // Define click listener for the ViewHolder's View.
+            v.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (data != null) {
+
+                    }
+                }
+            });
+            titleView = v.findViewById(R.id.tv_name);
+            contentView = v.findViewById(R.id.tv_desctiption);
+            imageView = v.findViewById(R.id.iv_media);
+        }
+    }
+    // END_INCLUDE(recyclerViewSampleViewHolder)
+
+    /**
+     * Initialize the dataset of the Adapter.
+     *
+     * @param dataSet String[] containing the data to populate views to be used by RecyclerView.
+     */
+    public ContentAdapter(List<Map> dataSet) {
+        mDataSet = dataSet;
+    }
+
+    // BEGIN_INCLUDE(recyclerViewOnCreateViewHolder)
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view.
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.fragment_content, viewGroup, false);
+
+        return new ViewHolder(v);
+    }
+    // END_INCLUDE(recyclerViewOnCreateViewHolder)
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        // Get element from your dataset at this position and replace the contents of the view
+        // with that element
+        viewHolder.titleView.setText(mDataSet.get(position).get("title").toString());
+        viewHolder.contentView.setText("Rp " + mDataSet.get(position).get("content").toString());
+        viewHolder.data = mDataSet.get(position);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+}
